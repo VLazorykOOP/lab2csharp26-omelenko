4079709c28b966c53b1a3168166b43bf7829e1b1
omelenko <melenko.oleksandr@chnu.edu.ua>
2026-03-12 21:06:39 +0200

    lab2 + tests

---
+++ Lab2CSharp/Lab2CSharp.Tests/Lab2CSharpTests.cs
@@ -0,0 +1,77 @@
+﻿using Xunit;
+
+namespace Lab2CSharp.Tests;
+
+public class ProgramTests
+{
+    [Fact]
+    public void Task1_ShouldReturnCorrectOddIndices()
+    {
+        // Симулюємо ввід: Розмір 5, Елементи: 10, 11, 12, 13, 14
+        var input = string.Join(Environment.NewLine, new[] { "5", "10", "11", "12", "13", "14" });
+        using var reader = new StringReader(input);
+        Console.SetIn(reader);
+
+        var result = Program.Task1();
+
+        Assert.Equal(new int[] { 1, 3 }, result);
+    }
+
+    [Fact]
+    public void Task2_ShouldReturnMaxNegativeValue()
+    {
+        // 2 рядки, 2 стовпці: -10, 5, -2, 0
+        var input = string.Join(Environment.NewLine, new[] { "2", "2", "-10", "5", "-2", "0" });
+        using var reader = new StringReader(input);
+        Console.SetIn(reader);
+
+        var result = Program.Task2();
+
+        Assert.Equal(-2, result);
+    }
+
+    [Fact]
+    public void Task3_ShouldReturnSnakePattern()
+    {
+        /* Матриця 2x2
+           1 2
+           3 4
+           Очікуємо "Змійку": 1, 2 (вправо), потім 4, 3 (вліво)
+        */
+        var input = string.Join(Environment.NewLine, new[] { "2", "1", "2", "3", "4" });
+        using var reader = new StringReader(input);
+        Console.SetIn(reader);
+
+        var result = Program.Task3();
+
+        Assert.Equal(new int[] { 1, 2, 4, 3 }, result);
+    }
+
+    [Fact]
+    public void Task4_ShouldCalculateRowSumsAndMaxSum()
+    {
+        /* 
+           Рядок 1: 2 елементи (1, 2) -> сума 3
+           Рядок 2: 3 елементи (10, 20, 30) -> сума 60
+        */
+        var input = string.Join(Environment.NewLine, new[] { "2", "2", "1", "2", "3", "10", "20", "30" });
+        using var reader = new StringReader(input);
+        Console.SetIn(reader);
+
+        var (sums, maxSum) = Program.Task4();
+
+        Assert.Equal(new int[] { 3, 60 }, sums);
+        Assert.Equal(60, maxSum);
+    }
+
+    [Fact]
+    public void Task2_NoNegatives_ShouldThrowException()
+    {
+        // Всі числа додатні
+        var input = string.Join(Environment.NewLine, new[] { "1", "1", "10" });
+        using var reader = new StringReader(input);
+        Console.SetIn(reader);
+
+        Assert.Throws<InvalidOperationException>(() => Program.Task2());
+    }
+}
--- Lab2CSharp/Program.cs
+++ Lab2CSharp/Program.cs
@@ -1,10 +1,190 @@
-﻿namespace Lab2CSharp
+﻿namespace Lab2CSharp;
+
+using System;
+using System.Collections.Generic;
+
+public class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        Print("Виберiть завдання:");
+        Print("1. Вивести номери непарних елементiв");
+        Print("2. Знайти максимум з вiд’ємних елементiв");
+        Print("3. Вивести елементи матрицi в стилi змiйки");
+        Print("4. Пiдрахувати суму елементiв кожного рядка i знайти максимальний елемент нового масиву");
+
+        int choice = Convert.ToInt32(Console.ReadLine());
+        switch (choice)
         {
-            Console.WriteLine("Lab 2 CSharp");
+            case 1:
+                PrintArray(Task1());
+                break;
+            case 2:
+                Print(Task2().ToString());
+                break;
+            case 3:
+                PrintArray(Task3());
+                break;
+            case 4:
+                PrintTask4Results(Task4());
+                break;
+            default:
+                Print("Невiрний вибiр.");
+                break;
         }
+    }
+
+    static void Print(string message, bool write = false)
+    {
+        if (write) Console.Write(message);
+        else Console.WriteLine(message);
+    }
+
+    static void PrintArray(int[] array)
+    {
+        Print("Результат: " + string.Join(", ", array));
+    }
+
+    static void PrintTask4Results((int[] sums, int maxSum) results)
+    {
+        Print("Сума елементiв кожного рядка:");
+        PrintArray(results.sums);
+        Print($"Максимальна сума: {results.maxSum}");
+    }
+
+    public static int[] Task1()
+    {
+        Print("Введiть розмiр масиву: ", true);
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] array = new int[n];
+
+        Print("Введiть елементи масиву:");
+        for (int i = 0; i < n; i++)
+        {
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        List<int> oddIndices = new List<int>();
+        for (int i = 0; i < n; i++)
+        {
+            if (array[i] % 2 != 0)
+            {
+                oddIndices.Add(i);
+            }
+        }
+        return oddIndices.ToArray();
+    }
+
+    public static int Task2()
+    {
+        Print("Введiть кiлькiсть рядкiв: ", true);
+        int rows = Convert.ToInt32(Console.ReadLine());
+        Print("Введiть кiлькiсть стовпцiв: ", true);
+        int cols = Convert.ToInt32(Console.ReadLine());
+        int[,] array = new int[rows, cols];
+
+        Print("Введiть елементи масиву:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                array[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        int maxNegative = int.MinValue;
+        bool hasNegative = false;
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                if (array[i, j] < 0)
+                {
+                    hasNegative = true;
+                    if (array[i, j] > maxNegative)
+                    {
+                        maxNegative = array[i, j];
+                    }
+                }
+            }
+        }
+
+        if (hasNegative)
+        {
+            return maxNegative;
+        }
+        else
+        {
+            throw new InvalidOperationException("В масивi немає вiд’ємних елементiв.");
+        }
+    }
+
+    public static int[] Task3()
+    {
+        Print("Введiть розмiр матрицi (n x n): ", true);
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[,] matrix = new int[n, n];
+
+        Print("Введiть елементи матрицi:");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        List<int> snakeElements = new List<int>();
+        for (int i = 0; i < n; i++)
+        {
+            if (i % 2 == 0) // парний рядок
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    snakeElements.Add(matrix[i, j]);
+                }
+            }
+            else // непарний рядок
+            {
+                for (int j = n - 1; j >= 0; j--)
+                {
+                    snakeElements.Add(matrix[i, j]);
+                }
+            }
+        }
+        return snakeElements.ToArray();
+    }
+
+    public static (int[] sums, int maxSum) Task4()
+    {
+        Print("Введiть кiлькiсть рядкiв: ", true);
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[][] jaggedArray = new int[n][];
+        int[] sums = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Print($"Введiть кiлькiсть елементiв в рядку {i + 1}: ", true);
+            int m = Convert.ToInt32(Console.ReadLine());
+            jaggedArray[i] = new int[m];
+
+            Print($"Введiть елементи рядка {i + 1}:");
+            for (int j = 0; j < m; j++)
+            {
+                jaggedArray[i][j] = Convert.ToInt32(Console.ReadLine());
+                sums[i] += jaggedArray[i][j];
+            }
+        }
+
+        int maxSum = sums[0];
+        for (int i = 1; i < sums.Length; i++)
+        {
+            if (sums[i] > maxSum)
+            {
+                maxSum = sums[i];
+            }
+        }
+        return (sums, maxSum);
     }
 }
